e9cd339759cf0f082d2743f4c7c5d76f13edeb35
Jana Sy <c22363431@mytudublin.ie>
2025-11-23 03:40:07 +0000

    successfully connected django project to psql db

--- backend/checkpoint/settings.py
+++ backend/checkpoint/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'checkpoint.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'checkpoint_db',
+        'USER': 'checkpoint_user',
+        'PASSWORD': 'changeme123',
+        'HOST': 'localhost',
+        'PORT': '5433',
     }
 }
 
